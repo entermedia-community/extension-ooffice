3f1322e7774608b3043de179dd3590f5bb1f9b55
cburkey <cburkey@entermediasoftware.com>
2011-07-18 10:10:52 -0400

    Michael tweaks

--- etc/install.js
+++ etc/install.js
@@ -3,7 +3,7 @@ importPackage( Packages.java.util );
 importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
-var war = "http://dev.entermediasoftware.com/projects/entermedia-ooffice/ROOT.war";
+var war = "http://dev.entermediasoftware.com/jenkins/job/entermedia-ooffice/lastSuccessfulBuild/artifact/deploy/ROOT.war";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
