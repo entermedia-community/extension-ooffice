f9b69f2909dc01191ebd1e6f1e48b32908b9bac7
ian <ian@netevolved.com>
2016-03-07 11:35:23 -0500

    Documents are working!

--- src/org/entermediadb/ooffice/OofficeDocumentTranscoder.java
+++ src/org/entermediadb/ooffice/OofficeDocumentTranscoder.java
@@ -49,10 +49,14 @@ public class OofficeDocumentTranscoder extends BaseTranscoder
 		command.add("pdf:writer_pdf_Export");
 
 		command.add("-outdir");
-		//String dir = inOut.getDirectory();
+		
+		Page out = getPageManager().getPage(inOut.getPath());
+		String outfolder = out.getDirectory();
+		String dir = out.getDirectory();
+		dir = getPageManager().getPage(dir).getContentItem().getAbsolutePath();
 		//log.info("{$inOut} turns into ${dir}");
-		String dir = inStructions.getOutputFile().getAbsolutePath();
-		new File( dir ).mkdirs();
+		//String dir = inStructions.getOutputFile().getAbsolutePath();
+		//new File( dir ).mkdirs();
 		command.add(dir);
 		
 		command.add(input.getContentItem().getAbsolutePath());
@@ -65,7 +69,7 @@ public class OofficeDocumentTranscoder extends BaseTranscoder
 		{
 			String newname = PathUtilities.extractPageName(input.getName()) + ".pdf";
 			
-			Page tmpfile = getPageManager().getPage(dir+ "/" + newname);
+			Page tmpfile = getPageManager().getPage(outfolder+ "/" + newname);
 			if( !tmpfile.exists())
 			{
 				throw new OpenEditException("OpenOffice did not create output file " + tmpfile);
